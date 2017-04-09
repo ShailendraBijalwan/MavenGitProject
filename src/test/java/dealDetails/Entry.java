7e4ebf1aad7747ec8bd3a9164bf4428f5501bc1b
ShailendraBijalwan <shailu9759@gmail.com>
2017-04-09 15:04:48 +0530

    update code

--- src/test/java/dealDetails/Entry.java
+++ src/test/java/dealDetails/Entry.java
@@ -1,5 +1,8 @@
 package dealDetails;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
@@ -10,9 +13,13 @@ public class Entry {
 	public void testEntry(){
 		//System.setProperty("webdriver.gecko.driver", "C:\\selenium\\geckodriver.exe");
 		WebDriver driver =new FirefoxDriver();
-		driver.get("https://www.google.co.in");
+		driver.get("https://www.iobnet.co.in/ibanking/login.do");
 		driver.manage().window().maximize();
-		driver.quit();
+		driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
+		driver.findElement(By.name("loginId")).sendKeys("Shailu123");
+		driver.findElement(By.id("password")).sendKeys("Sh1234");
+		driver.findElement(By.xpath("//input[@class='button' and @value='Submit']")).click();
+		
 	}
 	
 }
