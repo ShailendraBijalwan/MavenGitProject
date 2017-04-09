9de5ed939325312eab357576cc49c4206a159ac5
ShailendraBijalwan <shailu9759@gmail.com>
2017-04-09 18:42:38 +0530

    added

--- src/test/java/dealDetails/Entry.java
+++ src/test/java/dealDetails/Entry.java
@@ -13,12 +13,21 @@ public class Entry {
 	public void testEntry(){
 		//System.setProperty("webdriver.gecko.driver", "C:\\selenium\\geckodriver.exe");
 		WebDriver driver =new FirefoxDriver();
-		driver.get("https://www.iobnet.co.in/ibanking/login.do");
+		/*driver.get("https://www.iobnet.co.in/ibanking/login.do");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
 		driver.findElement(By.name("loginId")).sendKeys("Shailu123");
 		driver.findElement(By.id("password")).sendKeys("Sh1234");
 		driver.findElement(By.xpath("//input[@class='button' and @value='Submit']")).click();
+		*/
+		System.out.println("hello world 1");
+		System.out.println("hello world 1");
+		System.out.println("hello world 1");
+		System.out.println("hello world 1");
+		System.out.println("hello world 1");
+		System.out.println("hello world 1");
+		System.out.println("hello world 1");
+		System.out.println("hello world 1");
 		
 	}
 	
